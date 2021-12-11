9d271118cf483310e597013278448cf839305bd3
Kaung Sithu <kgcthu.kev@gmail.com>
2021-12-11 15:29:09 +0630

    FeedbackForm and Button component

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import Button from './shared/Button'
+import Card from './shared/Card'
+
+function FeedbackForm() {
+  const [text, setText] = useState('')
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate our service?</h2>
+        {/* rating select component */}
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Write a review'
+            value={text}
+          />
+          <Button type='submit'>Send</Button>
+        </div>
+      </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
