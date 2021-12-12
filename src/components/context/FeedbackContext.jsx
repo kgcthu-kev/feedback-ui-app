80c8804e250cccce1c2f0ea82d54dd11119b3a66
Kaung Sithu <kgcthu.kev@gmail.com>
2021-12-12 20:22:06 +0630

    update feedback edit item

--- src/components/context/FeedbackContext.jsx
+++ src/components/context/FeedbackContext.jsx
@@ -45,14 +45,22 @@ export const FeedbackProvider = ({ children }) => {
     })
   }
 
+  // update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    )
+  }
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
+        updateFeedback,
       }}
     >
       {children}
